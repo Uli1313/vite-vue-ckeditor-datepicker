585289feaa488610debdbd16239967aabcf7dc21
Uli <ru21041555@gmail.com>
2024-03-08 16:25:25 +0800

    部屬設定

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/vite-vue-ckeditor-datepicker/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
